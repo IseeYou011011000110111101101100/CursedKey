edb87a3b891821ee80c31cf4560ec8040860fad3
IseeYou011011000110111101101100 <116695604+IseeYou011011000110111101101100@users.noreply.github.com>
2022-12-01 17:10:41 +0700

    Add files via upload

---
+++ Source.lua
@@ -0,0 +1,113 @@
+-- Services
+
+local Players = game:GetService("Players")
+local TS = game:GetService("TweenService")
+local ReSt = game:GetService("ReplicatedStorage")
+local PPS = game:GetService("ProximityPromptService")
+
+-- Variables
+
+local Plr = Players.LocalPlayer
+local Char = Plr.Character or Plr.CharacterAdded:Wait()
+local Hum = Char:WaitForChild("Humanoid")
+
+local SelfModules = {
+    Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))(),
+    DoorReplication = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Door%20Replication/Source.lua"))(),
+    Achievements = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Achievements/Source.lua"))(),
+    CustomShop = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Custom%20Shop%20Items/Source.lua"))(),
+}
+local Assets = {
+    KeyItem = LoadCustomInstance("https://github.com/RegularVynixu/Miscellaneous/blob/main/Doors/Cursed%20Key/CursedKeyItem.rbxm?raw=true"),
+}
+
+-- Functions
+
+local function replicateDoor(room)
+    local originalDoor = room:FindFirstChild("Door")
+
+    if originalDoor then
+        local door = SelfModules.DoorReplication.CreateDoor({
+            Locked = room:WaitForChild("Assets"):WaitForChild("KeyObtain", 0.3) ~= nil,
+            Sign = true,
+            Light = true,
+            Barricaded = false,
+            CustomKeyNames = {"CursedKey"},
+            DestroyKey = false,
+            GuidingLight = true,
+            FastOpen = false,
+        })
+
+        door.Model.Name = "FakeDoor"
+        door.Model:SetPrimaryPartCFrame(originalDoor.PrimaryPart.CFrame)
+        door.Model.Parent = room
+        SelfModules.DoorReplication.ReplicateDoor(door)
+        originalDoor:Destroy()
+        
+        door.Debug.OnDoorOpened = function()
+            local key = Char:FindFirstChild(Assets.KeyItem.Name) or Char:FindFirstChild("Key")
+
+            if key then
+                if key.Name == Assets.KeyItem.Name then
+                    local uses = key:GetAttribute("Uses") - 1
+    
+                    if uses == 0 then
+                        key:Destroy()
+    
+                        SelfModules.Achievements.Get({
+                            Title = "Unbolting Hazard",
+                            Desc = "Indefinitely cursing yourself.",
+                            Reason = "Breaking the Cursed Key.",
+                            Image = "https://media.discordapp.net/attachments/1035320391142477896/1036335501004779632/unknown.png",
+                        })
+                    else
+                        key:SetAttribute("Uses", uses)
+                    end
+    
+                    Hum.Health = math.max(Hum.Health - 10, 0)
+                    workspace.Curse:Play()
+                else
+                    key:Destroy()
+                end
+            end
+        end
+    end
+end
+
+-- Scripts
+
+if typeof(Assets.KeyItem) ~= "Instance" then
+    return
+end
+
+Assets.KeyItem.Curse.Parent = workspace
+
+-- Door replication setup
+
+task.spawn(function()
+    for _, v in next, workspace.CurrentRooms:GetChildren() do
+        if v:FindFirstChild("Door") and v.Door:FindFirstChild("Lock") then
+            replicateDoor(v)
+        end
+    end
+    
+    workspace.CurrentRooms.DescendantAdded:Connect(function(des)
+        if des.Name == "Lock" and des.Parent.Name == "Door" then
+            task.wait(0.3)
+
+            if des.Parent then
+                replicateDoor(des.Parent.Parent)
+            end
+        end
+    end)
+end)
+
+-- Obtain cursed key
+
+SelfModules.CustomShop.CreateItem(Assets.KeyItem:Clone(), {
+    Title = "Cursed Key",
+    Desc = "Five uses, holds secrets",
+    Image = "https://media.discordapp.net/attachments/1035320391142477896/1035697972924661790/CursedKey.png",
+    Price = 100,
+    Stack = 1,
+})
